fc3914c1a76bc7b8014fc024d52d0ebe42611289
eddie <edavidmanotas@gmail.com>
2024-06-09 23:43:28 -0500

    parser version 2.0

--- backend/parser.py
+++ backend/parser.py
@@ -12,6 +12,9 @@ precedence = (
     ('nonassoc', 'LPAREN', 'RPAREN'),
 )
 
+# Diccionario de variables
+variables = {}
+
 # Definición de la gramática
 def p_program(p):
     '''program : statement_list'''
@@ -29,12 +32,12 @@ def p_statement(p):
     '''statement : expression END_LINE
                  | assignment END_LINE
                  | declaration END_LINE
-                 | conditional END_LINE
-                 | loop END_LINE
-                 | function_declaration END_LINE
+                 | conditional
+                 | loop
                  | return_statement END_LINE
                  | break_statement END_LINE
-                 | comment'''
+                 | comment
+                 '''
     p[0] = p[1]
 
 def p_expression(p):
@@ -47,23 +50,45 @@ def p_binary_expression(p):
     '''binary_expression : primary_expression PLUS_OP primary_expression
                          | primary_expression MINUS_OP primary_expression
                          | primary_expression MUL_OP primary_expression
-                         | primary_expression DIV_OP primary_expression'''
-    p[0] = {
-        'type': 'binary_expression',
-        'operator': p[2],
-        'left': p[1],
-        'right': p[3],
-        'result': eval(f"{p[1]['value']} {p[2]} {p[3]['value']}")
-    }
+                         | primary_expression DIV_OP primary_expression
+                         | primary_expression LESS_OP primary_expression
+                         | primary_expression GREATER_OP primary_expression
+                         | primary_expression LESS_EQUAL_OP primary_expression
+                         | primary_expression GREATER_EQUAL_OP primary_expression
+                         | primary_expression EQUAL_OP primary_expression
+                         | primary_expression DIFFERENT_OP primary_expression'''
+    left_value = p[1]['result']
+    right_value = p[3]['result']
+    if p[2] == '+':
+        result = left_value + right_value
+    elif p[2] == '-':
+        result = left_value - right_value
+    elif p[2] == '*':
+        result = left_value * right_value
+    elif p[2] == '/':
+        result = left_value / right_value
+    elif p[2] == '<':
+        result = left_value < right_value
+    elif p[2] == '>':
+        result = left_value > right_value
+    elif p[2] == '<=':
+        result = left_value <= right_value
+    elif p[2] == '>=':
+        result = left_value >= right_value
+    elif p[2] == '==':
+        result = left_value == right_value
+    elif p[2] == '!=':
+        result = left_value != right_value
+    p[0] = {'type': 'binary_expression', 'operator': p[2], 'left': p[1], 'right': p[3], 'result': result}
 
 def p_unitary_expression(p):
-    '''unitary_expression : MINUS_OP primary_expression'''
-    p[0] = {
-        'type': 'unitary_expression',
-        'operator': p[1],
-        'operand': p[2],
-        'result': -p[2]['value']
-    }
+    '''unitary_expression : MINUS_OP primary_expression
+                          | LOGICAL_OP_NOT primary_expression'''
+    if p[1] == '-':
+        result = -p[2]['result']
+    elif p[1] == 'FAKE':
+        result = not p[2]['result']
+    p[0] = {'type': 'unitary_expression', 'operator': p[1], 'operand': p[2], 'result': result}
 
 def p_primary_expression(p):
     '''primary_expression : LPAREN expression RPAREN
@@ -82,18 +107,23 @@ def p_term(p):
             | NULL
             | TEXT_STRING
             | TEXT_CHAR'''
-    p[0] = {
-        'type': 'term',
-        'value': p[1]
-    }
+    if isinstance(p[1], int) or isinstance(p[1], float):
+        p[0] = {'type': 'term', 'result': p[1]}
+    elif isinstance(p[1], str) and p[1] in variables:
+        p[0] = {'type': 'term', 'result': variables[p[1]]}
+    elif p[1] == 'VERUM':
+        p[0] = {'type': 'term', 'result': True}
+    elif p[1] == 'FALSUM':
+        p[0] = {'type': 'term', 'result': False}
+    elif p[1] == 'NIHIL':
+        p[0] = {'type': 'term', 'result': None}
+    else:
+        p[0] = {'type': 'term', 'result': p[1]}
 
 def p_assignment(p):
     '''assignment : VARIABLE ASSIGNMENT_OP expression'''
-    p[0] = {
-        'type': 'assignment',
-        'variable': p[1],
-        'value': p[3]
-    }
+    variables[p[1]] = p[3]['result']
+    p[0] = {'type': 'assignment', 'variable': p[1], 'value': p[3]['result']}
 
 def p_declaration(p):
     '''declaration : TYPE_BOOLEAN VARIABLE 
@@ -107,91 +137,91 @@ def p_declaration(p):
                    | TYPE_INTEGER assignment
                    | TYPE_FLOAT assignment'''
     if isinstance(p[2], dict) and p[2]['type'] == 'assignment':
-        p[0] = {
-            'type': 'declaration',
-            'data_type': p[1],
-            'variable': p[2]['variable'],
-            'value': p[2]['value']
-        }
+        variables[p[2]['variable']] = p[2]['value']
+        p[0] = {'type': 'declaration', 'data_type': p[1], 'variable': p[2]['variable'], 'value': p[2]['value']}
     else:
-        p[0] = {
-            'type': 'declaration',
-            'data_type': p[1],
-            'variable': p[2]
-        }
+        variables[p[2]] = None
+        p[0] = {'type': 'declaration', 'data_type': p[1], 'variable': p[2]}
 
 def p_conditional(p):
-    '''conditional : CONDITIONAL1 LPAREN expression RPAREN STRUCTURE_BODY statement_list
+    '''conditional : CONDITIONAL1 LPAREN expression RPAREN STRUCTURE_BODY statement_list 
                    | CONDITIONAL1 LPAREN expression RPAREN STRUCTURE_BODY statement_list CONDITIONAL2 STRUCTURE_BODY statement_list'''
     if len(p) == 7:
-        p[0] = {
-            'type': 'conditional',
-            'condition': p[3],
-            'if_body': p[6]
-        }
+        if p[3]['result']:
+            for stmt in p[6]:
+                execute_statement(stmt)
+        p[0] = {'type': 'conditional', 'condition': p[3], 'if_body': p[6]}
     else:
-        p[0] = {
-            'type': 'conditional',
-            'condition': p[3],
-            'if_body': p[6],
-            'else_body': p[9]
-        }
+        if p[3]['result']:
+            for stmt in p[6]:
+                execute_statement(stmt)
+        else:
+            for stmt in p[9]:
+                execute_statement(stmt)
+        p[0] = {'type': 'conditional', 'condition': p[3], 'if_body': p[6], 'else_body': p[9]}
 
 def p_loop(p):
-    '''loop : LOOP1 LPAREN expression RPAREN STRUCTURE_BODY statement_list
-            | LOOP2 LPAREN expression SEPARATION expression SEPARATION expression RPAREN STRUCTURE_BODY statement_list'''
+    '''loop : LOOP1 LPAREN expression RPAREN STRUCTURE_BODY statement_list 
+            | LOOP2 LPAREN assignment SEPARATION expression SEPARATION assignment RPAREN STRUCTURE_BODY statement_list'''
     if p[1] == 'ALPHA_LOOP':
-        p[0] = {
-            'type': 'while_loop',
-            'condition': p[3],
-            'body': p[6]
-        }
+        while p[3]['result']:
+            for stmt in p[6]:
+                execute_statement(stmt)
+        p[0] = {'type': 'while_loop', 'condition': p[3], 'body': p[6]}
     else:
-        p[0] = {
-            'type': 'for_loop',
-            'initialization': p[3],
-            'condition': p[5],
-            'increment': p[7],
-            'body': p[10]
-        }
-
-def p_function_declaration(p):
-    '''function_declaration : FUNCTION_DECLARATION VARIABLE LPAREN RPAREN STRUCTURE_BODY statement_list'''
-    p[0] = {
-        'type': 'function_declaration',
-        'name': p[2],
-        'body': p[6]
-    }
+        initialization = p[3]['result']
+        condition = p[5]['result']
+        increment = p[7]['result']
+        while condition:
+            for stmt in p[10]:
+                execute_statement(stmt)
+            execute_statement(increment)
+        p[0] = {'type': 'for_loop', 'initialization': p[3], 'condition': p[5], 'increment': p[7], 'body': p[10]}
 
 def p_return_statement(p):
     '''return_statement : RETURN expression
                         | RETURN'''
     if len(p) == 3:
-        p[0] = {
-            'type': 'return_statement',
-            'value': p[2]
-        }
+        p[0] = {'type': 'return', 'value': p[2]['result']}
     else:
-        p[0] = {
-            'type': 'return_statement',
-            'value': None
-        }
+        p[0] = {'type': 'return', 'value': None}
 
 def p_break_statement(p):
     '''break_statement : BREAK'''
-    p[0] = {
-        'type': 'break_statement'
-    }
+    p[0] = {'type': 'break'}
 
 def p_comment(p):
     '''comment : COMMENT'''
-    p[0] = {
-        'type': 'comment',
-        'value': p[1]
-    }
+    p[0] = {'type': 'comment', 'value': p[1]}
 
 def p_error(p):
     print(f"Syntax error at '{p.value}'")
 
 # Construcción del parser
 parser = yacc.yacc()
+
+def execute_statement(statement):
+    if statement['type'] == 'assignment':
+        variables[statement['variable']] = statement['value']
+    elif statement['type'] == 'declaration':
+        variables[statement['variable']] = statement['value']
+    elif statement['type'] == 'conditional':
+        if 'if_body' in statement:
+            for stmt in statement['if_body']:
+                execute_statement(stmt)
+        elif 'else_body' in statement:
+            for stmt in statement['else_body']:
+                execute_statement(stmt)
+    elif statement['type'] == 'loop':
+        if 'while_loop' == statement['type']:
+            while statement['condition']['result']:
+                for stmt in statement['body']:
+                    execute_statement(stmt)
+        elif 'for_loop' == statement['type']:
+            initialization = statement['initialization']
+            condition = statement['condition']
+            increment = statement['increment']
+            while condition['result']:
+                for stmt in statement['body']:
+                    execute_statement(stmt)
+                execute_statement(increment)
